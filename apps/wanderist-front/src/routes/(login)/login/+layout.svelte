f40ae0ae776b40be07d0b391624365664c948460
kyle <kaplaugher94@gmail.com>
2024-01-21 16:32:26 -0500

    error creating user

---
+++ apps/wanderist-front/src/routes/(login)/login/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let data;
+</script>
+
+<div class="h-full">
+	<slot><!-- optional fallback --></slot>
+</div>
--- apps/wanderist-front/src/routes/(register)/register/+page.server.ts
+++ apps/wanderist-front/src/routes/(register)/register/+page.server.ts
@@ -3,16 +3,21 @@ import { error, redirect } from '@sveltejs/kit';
 
 export const actions = {
 	register: async ({ locals, request }) => {
+		console.log('asdfasdfasdfasdfasfdasdfasdf -------------------');
 		const body = Object.fromEntries(await request.formData());
 
 		const username = generateUserName(body.name.split(' ').join('')).toLowerCase();
+
+		console.log('body: ', body);
+
+		console.log('username: ', username);
 
 		try {
 			await locals.pb.collection('users').create({ username, ...body });
 			await locals.pb.collection('users').requestVerification(body.email);
 		} catch (err) {
 			console.log('Error: ', err);
-			throw error(500, 'Something went wrong');
+			throw error(500, 'Something went wrong my dude');
 		}
 
 		throw redirect(303, '/login');
