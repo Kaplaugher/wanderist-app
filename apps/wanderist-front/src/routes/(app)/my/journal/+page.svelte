218e7a26fbd1df7f9b9e58be6c3ca61e269b33e7
kyle <kaplaugher94@gmail.com>
2024-01-25 15:35:25 -0500

    move create to own route

--- apps/wanderist-front/src/routes/(app)/my/journal/+page.svelte
+++ apps/wanderist-front/src/routes/(app)/my/journal/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import CreateEntryForm from './create-entry-form.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import CreateEntryForm from './create/create-entry-form.svelte';
 
 	export let data;
 </script>
@@ -20,7 +21,7 @@
 					Vel dolorem qui facilis soluta sint aspernatur totam cumque.
 				</p>
 			</div>
-			<CreateEntryForm {data} />
+			<Button href="/my/journal/create">New Entry +</Button>
 		</div>
 		<div
 			class="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3"
---
+++ apps/wanderist-front/src/routes/(app)/my/journal/create/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import CreateEntryForm from './create-entry-form.svelte';
+
+	export let data;
+</script>
+
+<div><CreateEntryForm {data} /></div>
